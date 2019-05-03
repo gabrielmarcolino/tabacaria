58e04160be67923a3841743f84b8834ae2deffc8
GABRIEL ALVES MARCOLINO <gabriel.amarcolino@senacsp.edu.br>
2019-05-03 11:33:53 -0300

    add projeto

---
+++ SmokeHoot/app/src/main/java/net/br/smokehoot/activity/CadastroActivity.java
@@ -0,0 +1,15 @@
+package net.br.smokehoot.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.br.smokehoot.R;
+
+public class CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
---
+++ SmokeHoot/app/src/main/java/net/br/smokehoot/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package net.br.smokehoot.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import net.br.smokehoot.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
